13737dda15f19afe898989840891c2a4b73881f6
Bruno <elbrunoconde@gmail.com>
2023-02-20 15:43:12 -0300

    img fix

--- src/components/Navbar/NavSup/NavSup.jsx
+++ src/components/Navbar/NavSup/NavSup.jsx
@@ -1,11 +1,12 @@
 import styles from "./NavSup.module.css"
 import Buscador from "./Buscador/Buscador.jsx"
 import CartWidget from "./CartWidget/CartWidget.jsx"
+import imgUrl from "../../../assets/img/logo/DynastyLogo.png" 
 
 const NavSup = () => {
   return (
     <div className={styles.NavSup}>
-      <img className={styles.dynastyLogo} src="./src/assets/img/logo/DynastyLogo.png" alt="" />
+      <img className={styles.dynastyLogo} src={imgUrl} alt="" />
       <Buscador></Buscador>
       <CartWidget></CartWidget>
     </div>
