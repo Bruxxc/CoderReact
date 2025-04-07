e9e80ff0855c5d5ad86bda053d09cc05fad8637f
Bruno <elbrunoconde@gmail.com>
2025-04-07 01:01:06 -0300

    a ver si se arregla

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Navbar from './components/Navbar/Navbar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import { useEffect,useState } from 'react'
-import './App.css'
+import './assets/App.css'
 import { Route,Routes } from 'react-router-dom';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import DefaultMenu from './components/DefaultMenu/DefaultMenu';
