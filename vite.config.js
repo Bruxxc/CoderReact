a1295ce899dd0c8af74687cc543c8192f8afe72e
Bruno <elbrunoconde@gmail.com>
2025-04-07 01:02:16 -0300

    arreglada ruta default

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // o simplemente elimina esta línea
+  base: '/CoderReact', // o simplemente elimina esta línea
 });
